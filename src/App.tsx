0fdecac7c5f246713443dbfe482f6f74082c6018
David Tarkhnishvili <datotarkh@gmail.com>
2025-07-08 14:50:10 +0400

    feat: implement state and form submission for search

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { Component } from 'react';
 import Container from './components/Container';
 import Button from './components/Button';
+import Search from './components/Search';
+
+export type Props = Record<string, never>;
 
 class App extends Component {
+  constructor(props: Props) {
+    super(props);
+    this.state = { searchTerm: '' };
+  }
+
+  handleSubmit(value: string) {
+    console.log('Submitted search:', value);
+    localStorage.setItem('searchTerm', JSON.stringify(value));
+    this.setState({ searchTerm: value });
+  }
   render() {
     return (
       <main className="bg-amber-200 w-full h-screen flex items-center justify-center">
         <Container>
           <div className="w-full">
             <h1 className="text-xl pb-[10px] text-center">Search</h1>
-            {/* Place for the Search component */}
+            <Search onSubmit={this.handleSubmit.bind(this)} />
           </div>
           <div className="w-full">
             <h1 className="text-xl pb-[10px] text-center">Results</h1>
