df8d8f5c7abe592af643fbed494532baf2a799c1
David Tarkhnishvili <datotarkh@gmail.com>
2025-07-09 12:59:27 +0400

    feat: add state and function to manually trigger an error

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ export type Props = Record<string, never>;
 
 type State = {
   searchTerm: string;
+  tirggerError: boolean;
 };
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { searchTerm: '' };
+    this.state = { searchTerm: '', tirggerError: false };
   }
 
   handleSubmit(value: string) {
@@ -21,7 +22,14 @@ class App extends Component<Props, State> {
     localStorage.setItem('searchTerm', JSON.stringify(value));
     this.setState({ searchTerm: value });
   }
+  onTriggerError() {
+    this.setState({ tirggerError: true });
+  }
   render() {
+    if (this.state.tirggerError) {
+      throw new Error('Manually triggered error');
+    }
+
     return (
       <main className="bg-amber-200 w-full h-screen flex items-center justify-center">
         <Container>
@@ -34,7 +42,9 @@ class App extends Component<Props, State> {
             <Results searchTerm={this.state.searchTerm} />
           </div>
           <div className="w-full flex justify-end">
-            <Button>Trigger Error</Button>
+            <Button onClick={this.onTriggerError.bind(this)}>
+              Trigger Error
+            </Button>
           </div>
         </Container>
       </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,15 @@
-export default function Button({ children }: { children: React.ReactNode }) {
+export default function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
   return (
-    <button className="bg-zinc-700 px-3 rounded-md text-zinc-200 py-[3px] hover:bg-zinc-600 transition-all ml-[3px]">
+    <button
+      onClick={onClick}
+      className="bg-zinc-700 px-3 rounded-md text-zinc-200 py-[3px] hover:bg-zinc-600 transition-all ml-[3px]"
+    >
       {children}
     </button>
   );
