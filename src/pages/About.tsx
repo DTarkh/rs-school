752e2449dba46296ca5739c8b360f22f306c1406
David Tarkhnishvili <datotarkh@gmail.com>
2025-07-21 12:28:41 +0400

    feat: add content on About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,32 @@
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <div className="mb-20">
+        <div className="mt-25">
+          <div className="flex flex-col sm:flex-row items-center gap-7">
+            <div className="h-[200px] w-[200px] rounded-full overflow-hidden">
+              <img src="/dd.jpg" alt="" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-zinc-700 text-3xl font-bold">
+                Hey, I&apos;m David. -{' '}
+                <span className="text-zinc-800">A React Developer</span>
+              </h3>
+              <h2 className="text-zinc-700 font-semibold text-2xl">
+                My github: Dtarkh{' '}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a
+        href="https://rs.school/courses/reactjs"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-purple-700 transition-all text-xl font-semibold text-zinc-600"
+      >
+        RS React Course
+      </a>
+    </>
+  );
 }
