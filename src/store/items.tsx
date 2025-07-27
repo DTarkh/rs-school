7549c6fd0d4b82f5a985fc8eaab1113bad630dba
David Tarkhnishvili <datotarkh@gmail.com>
2025-07-27 18:43:05 +0500

    feat: add reducer function for to empty selected items

--- src/store/items.tsx
+++ src/store/items.tsx
@@ -38,6 +38,9 @@ const itemsSlice = createSlice({
       state.items = state.items.filter((item) => item.id !== id);
       state.totalQuantity--;
     },
+    removeAllItems(state) {
+      state.items = [];
+    },
   },
 });
 
