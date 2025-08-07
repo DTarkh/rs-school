a1c68da6d7b43d3a5403bf20a89defb4a0ae3d51
David Tarkhnishvili <datotarkh@gmail.com>
2025-08-07 08:53:01 +0500

    init: migrate to Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Rs school project',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root"></div>
+        <script type="module" src="/src/main.tsx"></script>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
