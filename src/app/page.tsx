b54cc79facd31940dda5e9b4a98953ed5fcc160b
David Tarkhnishvili <datotarkh@gmail.com>
2025-08-09 23:43:52 +0500

    feat: preserve query params during navigation and add initial redirect with stored params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return redirect('/products');
+  if (typeof window !== 'undefined') {
+    const searchTerm = JSON.parse(localStorage.getItem('searchTerm') || '""');
+    const url =
+      searchTerm && searchTerm.trim() !== ''
+        ? `products?page=1&search=${searchTerm}`
+        : 'products?page=1&search=';
+
+    return redirect(url);
+  }
+
+  return null;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,24 @@
+'use client';
 import Link from 'next/link';
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation';
+
+function withCurrentQuery(
+  pathname: string,
+  sp: ReadonlyURLSearchParams | null
+) {
+  const query = sp ? sp.toString() : '';
+  return query ? `${pathname}?${query}` : pathname;
+}
 
 export default function Header() {
+  const searchParams = useSearchParams();
   return (
     <header className="bg-white/80 backdrop-blur-md shadow-lg border-b border-gray-100 z-50 w-full fixed top-0 flex justify-center items-center h-[60px]">
       <nav className="layout w-full flex justify-between items-center">
         <ul className="flex items-center justify-center gap-2 py-4">
           <li>
             <Link
-              href="/products"
+              href={withCurrentQuery('/products', searchParams)}
               className="flex items-center gap-1 px-4 py-2 rounded-lg font-semibold transition-all duration-300 text-gray-700 hover:text-purple-600 hover:bg-purple-50"
             >
               Products
@@ -15,7 +26,7 @@ export default function Header() {
           </li>
           <li>
             <Link
-              href="/about"
+              href={withCurrentQuery('/about', searchParams)}
               className="flex items-center gap-1 px-4 py-2 rounded-lg font-semibold transition-all duration-300 text-gray-700 hover:text-purple-600 hover:bg-purple-50"
             >
               About
