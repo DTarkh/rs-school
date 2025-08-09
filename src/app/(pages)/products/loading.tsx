e84482b4992b11664fb1250c91e799454f3a6ea3
David Tarkhnishvili <datotarkh@gmail.com>
2025-08-09 12:26:54 +0500

    feat: add loading to Results page

---
+++ src/app/(pages)/products/loading.tsx
@@ -0,0 +1,26 @@
+import Button from '../../../components/Button';
+import Search from '../../../components/Search';
+import Spinner from '../../../components/Spinner';
+
+export default function Loading() {
+  return (
+    <>
+      <div className="layout border p-4 rounded-xl border-gray-400 bg-white mt-[75px]">
+        <Search />
+      </div>
+
+      <div className="layout mt-[20px]">
+        <div className="flex justify-between items-center pb-3">
+          <h1 className="text-2xl pb-[10px] text-start font-semibold">
+            Results
+          </h1>
+          <Button>Trigger Error</Button>
+        </div>
+
+        <div className="border rounded-md  p-3 h-[600px] flex justify-center items-center">
+          <Spinner />
+        </div>
+      </div>
+    </>
+  );
+}
