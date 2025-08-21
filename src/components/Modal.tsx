2e83ee4089c647c58bae19714d4e32a49a9b2515
David Tarkhnishvili <datotarkh@gmail.com>
2025-08-21 23:00:25 +0400

    feat: close modal on backdrop click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -23,11 +23,16 @@ export default function Modal({
     }
   }, [open]);
 
+  function handleBackdropMouseDown(e: React.MouseEvent<HTMLDialogElement>) {
+    if (e.target === e.currentTarget) setOpen(false);
+  }
+
   return createPortal(
     <dialog
       className="min-w-[400px] bg-gray-300 backdrop:bg-black/50"
       ref={modalRef}
       onClose={() => setOpen(false)}
+      onMouseDown={handleBackdropMouseDown}
     >
       {children}
     </dialog>,
